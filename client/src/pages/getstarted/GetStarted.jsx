202baca369ae2d1f45bb930c5cc599c1fae81184
Shashankan-PKS <pksshashankan2003@gmail.com>
2025-11-14 12:57:05 +0530

    Logo-size-Change

--- client/src/pages/getstarted/GetStarted.jsx
+++ client/src/pages/getstarted/GetStarted.jsx
@@ -19,14 +19,14 @@ function GetStarted() {
     <>
       <Box sx={{display: "flex",flexDirection: "column", textAlign: "center",alignItems: "center",justifyContent: "space-between", height:"90vh" }}>
         <Box sx={{flexGrow: 1, display : "flex",alignItems: "center",justifyContent: "center",flexDirection : "column" }}>
-          <img src={logo} alt="Logo" style={{width:"1000px", maxWidth: "100%" }}/>
+          <img src={logo} alt="Logo" style={{width:"500px", maxWidth: "100%" }}/>
           <Box>
-            <CircularProgress enableTrackSlot size={40} style={{color : "#0D1B4C"}} />
+            <CircularProgress enableTrackSlot size={40} style={{color : "#4B0082"}} />
           </Box>
         </Box>
         
         <Box  sx={{ paddingBottom: "10px" }}>
-          <Typography sx={{color : "rgba(25, 42, 86)",fontFamily: "Epilogue"}}>Powered by <span style={{fontWeight: "800"}}> Techbrain Networks </span></Typography>
+          <Typography sx={{color : "#4B0082",fontFamily: "Epilogue"}}>Powered by <span style={{fontWeight: "800"}}> Shashankan </span></Typography>
         </Box>
       </Box>
     </>
